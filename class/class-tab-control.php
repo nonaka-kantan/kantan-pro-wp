da04c05cf77443211b1821d46ffbda2128f45c0b
nonaka <info@kantan-pro.com>
2022-11-28 21:53:50 +0900

    ログインログアウトURL

--- class/class-tab-control.php
+++ class/class-tab-control.php
@@ -8,18 +8,20 @@ class Kntan_Tab_Class {
     // }
 
     public function Tab_View( $tab_name ) {
+        $link = wp_logout_url();
         $content = <<<END
         <h3>ここは [$tab_name] です。</h3>
-        <p><font size="4"><a href="/$tab_name/>$tab_name">$tab_name</a></font></p>
+        <p><font size="4">ここに<a href="/$tab_name">$tab_name</a>の処理が入ります。</font></p>
+        <p><font size="4"><a href="$link">ログアウト</a></font></p>
         END;
         return $content;
     }
 
     public function Tab_Error( $tab_name ) {
+        $link = wp_login_url(); 
         $content = <<<END
         <h3>ログインしてください</h3>
-        <p><font size="4"><a href="https://kantanpro.local/ktp/wp-login.php">ログイン</a></font></p>
-        
+        <p><font size="4"><a href="$link">ログイン</a></font></p>
         END;
         return $content;
     }
